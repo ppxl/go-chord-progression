361f3cd3daca58421669378168c2f758350cf293
Philipp Pixel <boris.schaa@cloudogu.com>
2025-04-04 18:25:58 +0200

    idea, not working

--- api/api.go
+++ api/api.go
@@ -1,7 +1,9 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 )
 
 const (
@@ -11,7 +13,25 @@ const (
 
 // ServeAudio serves the chord progression audio file stream.
 func ServeAudio(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "sample.wav")
+	w.Header().Set("Content-Type", "audio/vnd.wave")
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		http.Error(w, "Streaming unsupported!", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Connection", "Keep-Alive")
+	w.Header().Set("Transfer-Encoding", "chunked")
+
+	for i := 0; i < 30; i++ {
+		_, err := fmt.Fprintf(w, "data: %d\n", i) // stream audio here
+		if err != nil {
+			http.Error(w, "error while writing to streaming", http.StatusInternalServerError)
+		}
+		flusher.Flush()
+		time.Sleep(300 * time.Millisecond)
+	}
+	// fixme, read file into a buffer and try to stream it slowly
+	http.ServeFile(w, r, "sample.mp3")
 }
 
 // ServeStatic serves all static files on the root route, f. i. the main page.
